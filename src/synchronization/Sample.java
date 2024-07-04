82d414a0f6d25487d78a202e622f07432e0f0872
Srivalli <snc.srivalli1@gmail.com>
2024-07-04 06:16:57 +0530

    Added locators notes of 3rd and 4th July

---
+++ src/synchronization/Sample.java
@@ -0,0 +1,17 @@
+package synchronization;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://shoppersstack.com/");
+		
+		driver.findElement(By.id("loginBtn")).click();
+	}
+
+}
